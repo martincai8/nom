588712fa3d903c91e9283d662c75f852471d3bee
martincai8 <martincai46@gmail.com>
2024-05-19 05:43:50 -0700

    Fix viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,31 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next';
+import './globals.css';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import { AuthProvider } from '@/utility/Auth'
+import { AuthProvider } from '@/utility/Auth';
 
 export const metadata: Metadata = {
-  title: "nom nom nom",
-  description: "nom",
+	title: 'nom nom nom',
+	description: 'nom',
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-
-  return (
-    <html lang="en">
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-      </head>
-      <body>
-        <AuthProvider>
-          {children}
-        </AuthProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang='en'>
+			<head>
+				<meta
+					name='viewport'
+					content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no viewport-fit=cover'
+				/>
+				<meta name='apple-mobile-web-app-capable' content='yes'></meta>
+				<meta name='apple-mobile-web-app-status-bar-style' content='black-translucent'></meta>
+			</head>
+			<body>
+				<AuthProvider>{children}</AuthProvider>
+			</body>
+		</html>
+	);
 }
