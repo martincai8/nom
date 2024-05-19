2c14fbb6034d6bc1e0a702d5822d4902722d426c
naijwu <jaewuchun@gmail.com>
2024-05-19 03:54:20 -0700

    add empty meals page

--- src/app/(auth)/alerts/page.tsx
+++ src/app/(auth)/alerts/page.tsx
@@ -1,12 +1,25 @@
 import { Settings, Profile } from '@/components/Profile/Profile';
 import styles from './page.module.css'
 import MiniYommer from "@/drawings/MiniYommer";
+import JumpingGommer from '@/drawings/JumpingGommer';
+import Button from '@/components/Button/Button';
 
 export default function AlertsPage() {
 
     return (
         <div className={styles.wrapper}>
-            <MiniYommer />
+            <div className={styles.centre}>
+                <h2> You currently don{`'`}t have any active meals</h2>
+                <JumpingGommer />
+                <div className={styles.cta}>
+                    <p>
+                        Hungry for more?
+                    </p>
+                    <Button>
+                        Plan a meal
+                    </Button>
+                </div>
+            </div>
         </div>
     )
 }
