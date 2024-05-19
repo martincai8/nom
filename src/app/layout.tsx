ddbbbea1d7168275572e62d6e424df9baad551e6
naijwu <jaewuchun@gmail.com>
2024-05-18 23:41:06 -0700

    lightwork

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta name="viewport" content="user-scalable=no"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
       </head>
       <body className={inter.className}>
         <AuthProvider>
