91bbd406684879eff5dfa31e578f17d097b570d2
martincai8 <martincai46@gmail.com>
2024-05-19 01:53:06 -0700

    pls

--- src/app/(auth)/page.tsx
+++ src/app/(auth)/page.tsx
@@ -19,7 +19,8 @@ export default function Home() {
 
   return !user ?
     !signingIn ?
-      <WelcomeCarousel onNext={()=>setSigningIn(true)} />
+      // <WelcomeCarousel onNext={()=>setSigningIn(true)} />
+      <SetMeetup />
       : <Landing />
     : !isOnboarded ?
       <Onboarding/>  
