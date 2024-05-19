007479227e571c264d0aaac386a075feea2bd734
naijwu <jaewuchun@gmail.com>
2024-05-19 03:30:56 -0700

    added hk, removed grey shadow on focus

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import { Inter, Space_Grotesk } from "next/font/google";
 import { AuthProvider } from '@/utility/Auth'
-
-const inter = Inter({ subsets: ["latin"] });
-const space = Space_Grotesk({ subsets: ["latin"]});
 
 export const metadata: Metadata = {
   title: "nom nom nom",
@@ -23,7 +19,7 @@ export default function RootLayout({
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
       </head>
-      <body className={inter.className}>
+      <body>
         <AuthProvider>
           {children}
         </AuthProvider>
