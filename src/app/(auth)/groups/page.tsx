d03867d760977c0c5dc7e95226607686c48cd75e
Joanne Lee <joannelee@Joannes-Laptop.local>
2024-05-19 05:52:15 -0700

    add design for group landing

--- src/app/(auth)/groups/page.tsx
+++ src/app/(auth)/groups/page.tsx
@@ -7,6 +7,7 @@ import Groups from '@/components/Groups/Groups';
 import Button from '@/components/Button/Button';
 import { useAuth } from '@/utility/Auth';
 import { getAllGroups } from '@/utility/firebase';
+import NommerGroup from '@/drawings/nommerGroup';
 
 export default function GroupsPage() {
 
@@ -33,10 +34,18 @@ export default function GroupsPage() {
 
     return !creating ? (
         <div className={styles.wrapper}>
-            <MiniYommer />
-            <h1>
-                My groups
-            </h1>
+            {/* <MiniYommer /> */}
+            <div className={styles.horWrapper}>
+                <h1>
+                    Your groups
+                </h1>
+                <Button onClick={()=>setCreating(true)}>
+                    Create group
+                </Button>
+            </div>
+            <p className={styles.subText}>
+                It’s looking a little lonely here. <br />Try making a new group!
+            </p>
             <div>
                 {groups?.map((g, index) => (
                     <div key={g}>
@@ -44,9 +53,9 @@ export default function GroupsPage() {
                     </div>
                 ))}
             </div>
-            <Button onClick={()=>setCreating(true)}>
-                Create group
-            </Button>
+            <div className={styles.nommerGroup}>
+                <NommerGroup />
+            </div>
         </div>
     ) : <Groups onFinish={()=>setCreating(false)} />
 }
